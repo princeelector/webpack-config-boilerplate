41830dde2782009415a4c53dd314fdd22868a9f8
Hubert Sikorski <sikorski.sr@gmail.com>
2019-12-30 01:44:49 +0100

    config part 3 - webpack server setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,28 @@
 const path = require('path');
 
+/**
+ * [hash]
+ * [chunkhash]
+ * [name]
+ * [id]
+ * [query]
+ * [contenthash]
+*/
+
+
 module.exports = {
     // this is actually the configuration webpack uses
     mode: 'development',
-    entry: './src/index.js', // webpack already looks for entry point in index.js by default
+    entry: {
+        main: './src/index.js'
+    }, // webpack already looks for entry point in index.js by default
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'build.js'
+        filename: 'js/[name].js',
+        publicPath: '/assets/'
+    },
+    devServer: {
+        port: 1234,
+        contentBase: path.join(__dirname, 'dist')
     }
 }
